337a8cc9de0140cf9b8523080608f98d84df6974
painterpuppets <painterpuppets@gmail.com>
2024-07-11 11:15:12 +0800

    chore: fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import bgImg from "./bg.png";
 import { Subscribe } from "./Subscribe";
 import { InfoDialog } from "./InfoDialog";
-import { PingPoint } from "./PingPoint";
-
 
 export default function Home() {
   return (
@@ -36,7 +34,19 @@ export default function Home() {
         />
 
         <InfoDialog>
-          <PingPoint className="top-[36%] left-[51.5%] cursor-pointer" />
+          <div
+            className={
+              "top-[36%] left-[51.5%] cursor-pointer transition-all duration-500 w-6 h-6 absolute flex items-center justify-center"
+            }
+          >
+            <span
+              className={
+                "animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"
+              }
+              style={{ animationDuration: "2200ms" }}
+            />
+            <span className="relative inline-flex rounded-full h-4 w-4 bg-white" />
+          </div>
         </InfoDialog>
       </div>
 
@@ -45,18 +55,30 @@ export default function Home() {
       <div className="flex mt-auto w-full md:px-8 px-4 md:text-base text-sm">
         <div className="mr-auto">
           PRESENTED BY{" "}
-          <a className="underline" href="https://www.nervos.org/foundation" target="_blank">
+          <a
+            className="underline"
+            href="https://www.nervos.org/foundation"
+            target="_blank"
+          >
             NERVOS FOUNDATION
           </a>
         </div>
         <div className="min-w-[60px] flex items-center">© 2024</div>
         <div className="ml-auto">
           BUILT BY{" "}
-          <a className="underline" href="https://www.nervape.com/" target="_blank">
+          <a
+            className="underline"
+            href="https://www.nervape.com/"
+            target="_blank"
+          >
             NERVAPE STUDIO
           </a>{" "}
           &{" "}
-          <a className="underline" href="https://www.magickbase.com/" target="_blank">
+          <a
+            className="underline"
+            href="https://www.magickbase.com/"
+            target="_blank"
+          >
             MAGICKBASE
           </a>
         </div>
