4018330b32165ac0e3387f6777ec9a60e1f413f9
Keith <keithwhisper@gmail.com>
2024-09-03 01:53:32 +0900

    feat: update partners

---
+++ src/app/2024/partners/list.ts
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: 'Nervos Foundation',
+    logo: '/img/partners/nervos.svg'
+  },
+  {
+    title: 'Cryptape',
+    logo: '/img/partners/cryptape.svg'
+  },
+  {
+    title: 'D.ID',
+    logo: '/img/partners/did.svg'
+  },
+  {
+    title: 'Polycrypt',
+    logo: '/img/partners/polycrypt.svg'
+  },
+  {
+    title: 'NFT Nation',
+    logo: '/img/partners/nftnation.svg'
+  },
+  {
+    title: 'Nervape',
+    logo: '/img/partners/nervape.svg'
+  },
+  {
+    title: 'Magickbase',
+    logo: '/img/partners/magickbase.svg'
+  },
+  {
+    title: 'Stable++',
+    logo: '/img/partners/stablepp.svg'
+  },
+]
--- src/app/2024/partners/page.tsx
+++ src/app/2024/partners/page.tsx
@@ -4,17 +4,7 @@ import { Footer } from "@/components/Footer";
 import { Banner } from "@/components/Banner";
 import { Subscribe } from "@/components/Subscribe";
 
-import ckbEcoFundLogo from "./logo/ckb-eco-fund.png";
-import cryptapeLogo from "./logo/cryptape.png";
-import didLogo from "./logo/did.png";
-import magickbaseLogo from "./logo/magickbase.png";
-import nervapeLogo from "./logo/nervape.png";
-import nervosLogo from "./logo/nervos.png";
-import nftnationLogo from "./logo/nftnation.png";
-import omigaLogo from "./logo/omiga.png";
-import polycryptLogo from "./logo/polycrypt.png";
-import sporeLogo from "./logo/spore.png";
-import stableppLogo from "./logo/stablepp.png";
+import PARTNERS from './list'
 
 export default function PartnersPage() {
   return (
@@ -25,83 +15,7 @@ export default function PartnersPage() {
       />
 
       <div className="flex flex-col items-center gap-20 xl:grid grid-cols-4">
-        <Image
-          src={nervosLogo.src}
-          height={nervosLogo.height}
-          width={nervosLogo.width}
-          alt="nervos"
-          priority
-        />
-        <Image
-          src={cryptapeLogo.src}
-          height={cryptapeLogo.height}
-          width={cryptapeLogo.width}
-          alt="cryptape"
-          priority
-        />
-        <Image
-          src={polycryptLogo.src}
-          height={polycryptLogo.height}
-          width={polycryptLogo.width}
-          alt="polycrypt"
-          priority
-        />
-        <Image
-          src={didLogo.src}
-          height={didLogo.height}
-          width={didLogo.width}
-          alt="did"
-          priority
-        />
-        <Image
-          src={nftnationLogo.src}
-          height={nftnationLogo.height}
-          width={nftnationLogo.width}
-          alt="nftnation"
-          priority
-        />
-        <Image
-          src={nervapeLogo.src}
-          height={nervapeLogo.height}
-          width={nervapeLogo.width}
-          alt="nervape"
-          priority
-        />
-        <Image
-          src={stableppLogo.src}
-          height={stableppLogo.height}
-          width={stableppLogo.width}
-          alt="stablepp"
-          priority
-        />
-        <Image
-          src={ckbEcoFundLogo.src}
-          height={ckbEcoFundLogo.height}
-          width={ckbEcoFundLogo.width}
-          alt="ckbEcoFund"
-          priority
-        />
-        <Image
-          src={magickbaseLogo.src}
-          height={magickbaseLogo.height}
-          width={magickbaseLogo.width}
-          alt="magickbase"
-          priority
-        />
-        <Image
-          src={omigaLogo.src}
-          height={omigaLogo.height}
-          width={omigaLogo.width}
-          alt="omiga"
-          priority
-        />
-        <Image
-          src={sporeLogo.src}
-          height={sporeLogo.height}
-          width={sporeLogo.width}
-          alt="spore"
-          priority
-        />
+        {PARTNERS.map(p => <Image key={p.title} width={162} height={46} src={p.logo} alt={p.title} title={p.title} priority />)}
       </div>
 
       <div className="flex flex-col items-center gap-8">
