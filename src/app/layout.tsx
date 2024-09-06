daab969911e3ccb8ee89320f594af4d9da4a1952
Keith <keithwhisper@gmail.com>
2024-09-06 22:36:24 +0900

    feat: adopt navigation globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { TRPCReactProvider } from "@/trpc/react";
 import { Inter } from "next/font/google";
+import Navigation from "@/components/Navigation";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <TRPCReactProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navigation />
+          {children}
+        </body>
       </TRPCReactProvider>
     </html>
   );
